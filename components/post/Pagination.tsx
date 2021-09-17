e33a72a6d3089f723bee9fe661ec959f628de69a
yrnana <nyryn0945@gmail.com>
2021-09-18 00:04:41 +0900

    feat: pagination 작성

--- components/post/Pagination.tsx
+++ components/post/Pagination.tsx
@@ -1,11 +1,115 @@
+import React, { useMemo } from 'react';
+import Link from 'next/link';
+import cx from 'classnames';
+import { LeftIcon, RightIcon } from '~/components/common/Icon';
+
+const pageItemClassName = 'w-10 py-2 text-center';
+const iconClassName = 'flex justify-center';
+
 const Pagination: React.VFC<Omit<Pageable<unknown>, 'data'>> = ({
   first,
   last,
-  size,
   page,
   totalPages,
 }) => {
-  return <>Pagination</>;
+  const pages = useMemo(
+    () => getPagination(totalPages, page),
+    [totalPages, page],
+  );
+
+  return (
+    <div className="flex flex-row flex-nowrap items-center justify-center mt-8">
+      {first ? (
+        <div className={cx('text-gray-300', pageItemClassName, iconClassName)}>
+          <LeftIcon />
+        </div>
+      ) : (
+        <Link href={`/page/${page - 1}`}>
+          <a className={cx(pageItemClassName, iconClassName)}>
+            <LeftIcon />
+          </a>
+        </Link>
+      )}
+      {pages.map((item, i) => (
+        <PaginationItem key={`${item.page}_${i}`} {...item} />
+      ))}
+      {last ? (
+        <div className={cx('text-gray-300', pageItemClassName, iconClassName)}>
+          <RightIcon />
+        </div>
+      ) : (
+        <Link href={`/page/${page + 1}`}>
+          <a className={cx(pageItemClassName, iconClassName)}>
+            <RightIcon />
+          </a>
+        </Link>
+      )}
+    </div>
+  );
 };
 
 export default Pagination;
+
+interface PaginationItemProps {
+  isPage: boolean;
+  isCurrentPage: boolean;
+  page: string | number;
+}
+
+const PaginationItem: React.VFC<PaginationItemProps> = ({
+  isPage,
+  isCurrentPage,
+  page,
+}) => {
+  if (isPage && !isCurrentPage) {
+    return (
+      <Link href={`/page/${page}`}>
+        <a className={pageItemClassName}>{page}</a>
+      </Link>
+    );
+  }
+  return (
+    <div
+      className={cx(pageItemClassName, 'cursor-default', {
+        'text-purple-500': isCurrentPage,
+      })}
+    >
+      {page}
+    </div>
+  );
+};
+
+function getPagination(
+  totalPages: number,
+  page: number,
+): PaginationItemProps[] {
+  if (totalPages <= 7) {
+    return Array.from({ length: totalPages }, (_, n) => ({
+      isPage: true,
+      isCurrentPage: n + 1 === page,
+      page: n + 1,
+    }));
+  }
+
+  let pages: (number | string)[];
+  if (page <= 4) {
+    pages = [1, 2, 3, 4, 5, '…', totalPages];
+  } else if (page >= totalPages - 3) {
+    pages = [
+      1,
+      '…',
+      totalPages - 4,
+      totalPages - 3,
+      totalPages - 2,
+      totalPages - 1,
+      totalPages,
+    ];
+  } else {
+    pages = [1, '…', page - 1, page, page + 1, '…', totalPages];
+  }
+  return pages.map((p) => ({
+    isPage: typeof p === 'number',
+    isCurrentPage: p === page,
+    page: p,
+  }));
+}
--- stories/components/Pagination.stories.tsx
+++ stories/components/Pagination.stories.tsx
@@ -1,9 +1,13 @@
 import type { ComponentStory, ComponentMeta } from '@storybook/react';
 import Pagination from '~/components/post/Pagination';
+import { PAGE_SIZE } from '~/helpers/constants';
 
 export default {
   title: 'components/post/Pagination',
   component: Pagination,
+  args: {
+    size: PAGE_SIZE,
+  },
 } as ComponentMeta<typeof Pagination>;
 
 const Template: ComponentStory<typeof Pagination> = (args) => (
@@ -15,21 +19,9 @@ Pages1Curr1.storyName = '페이지 1개 / 현재 1페이지';
 Pages1Curr1.args = {
   first: true,
   last: true,
-  size: 10,
   page: 1,
   totalElements: 5,
   totalPages: 1,
-};
-
-export const Pages5Curr1 = Template.bind({});
-Pages5Curr1.storyName = '페이지 5개 / 현재 1페이지';
-Pages5Curr1.args = {
-  first: true,
-  last: false,
-  size: 10,
-  page: 1,
-  totalElements: 45,
-  totalPages: 5,
 };
 
 export const Pages5Curr3 = Template.bind({});
@@ -37,21 +29,19 @@ Pages5Curr3.storyName = '페이지 5개 / 현재 3페이지';
 Pages5Curr3.args = {
   first: false,
   last: false,
-  size: 10,
   page: 3,
   totalElements: 45,
   totalPages: 5,
 };
 
-export const Pages5Curr5 = Template.bind({});
-Pages5Curr5.storyName = '페이지 5개 / 현재 5페이지';
-Pages5Curr5.args = {
-  first: false,
-  last: true,
-  size: 10,
-  page: 5,
-  totalElements: 45,
-  totalPages: 5,
+export const Pages8Curr1 = Template.bind({});
+Pages8Curr1.storyName = '페이지 8개 / 현재 1페이지';
+Pages8Curr1.args = {
+  first: true,
+  last: false,
+  page: 1,
+  totalElements: 75,
+  totalPages: 8,
 };
 
 export const Pages20Curr1 = Template.bind({});
@@ -59,7 +49,6 @@ Pages20Curr1.storyName = '페이지 20개 / 현재 1페이지';
 Pages20Curr1.args = {
   first: true,
   last: false,
-  size: 10,
   page: 1,
   totalElements: 195,
   totalPages: 20,
@@ -70,7 +59,6 @@ Pages20Curr10.storyName = '페이지 20개 / 현재 10페이지';
 Pages20Curr10.args = {
   first: false,
   last: false,
-  size: 10,
   page: 10,
   totalElements: 195,
   totalPages: 20,
@@ -81,7 +69,6 @@ Pages20Curr20.storyName = '페이지 20개 / 현재 20페이지';
 Pages20Curr20.args = {
   first: false,
   last: true,
-  size: 10,
   page: 20,
   totalElements: 195,
   totalPages: 20,
