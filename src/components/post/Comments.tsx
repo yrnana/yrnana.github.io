789f82b7bbc7440a5415b649214cbffba5c66016
yrnana <nyryn0945@gmail.com>
2021-09-21 06:32:06 +0900

    fix: comment 영역 스켈레톤 추가

--- src/components/post/Comments.tsx
+++ src/components/post/Comments.tsx
@@ -1,12 +1,25 @@
-import { useRef, useLayoutEffect, memo } from 'react';
+import { useRef, useEffect, useLayoutEffect, useState, memo } from 'react';
+import cx from 'classnames';
 import { COMMENT_ISSUE_REPO } from '~/helpers/constants';
 
-const Comments: React.VFC = memo(() => {
-  const divRef = useRef<HTMLDivElement>(null);
+// ssr support
+const useIsomorphicLayoutEffect =
+  typeof window !== 'undefined' ? useLayoutEffect : useEffect;
 
-  useLayoutEffect(() => {
-    const container = divRef.current;
+const Comments: React.VFC = memo(() => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useIsomorphicLayoutEffect(() => {
+    const container = containerRef.current;
     if (container) {
+      // observe container node
+      const observer = new MutationObserver((mutations) => {
+        setIsLoaded(mutations.length > 1);
+      });
+      observer.observe(container, { childList: true });
+
+      // add utteranc comment
       const script = document.createElement('script');
       script.setAttribute('src', 'https://utteranc.es/client.js');
       script.setAttribute('repo', COMMENT_ISSUE_REPO);
@@ -16,15 +29,42 @@ const Comments: React.VFC = memo(() => {
       script.setAttribute('crossorigin', 'anonymous');
       script.setAttribute('async', 'true');
       container.appendChild(script);
+      return () => {
+        container.querySelector('.utterances')?.remove();
+        observer.disconnect();
+      };
     }
-    return () => {
-      if (container) {
-        container.innerHTML = '';
-      }
-    };
   }, []);
 
-  return <div className="my-10" ref={divRef} />;
+  return (
+    <div
+      ref={containerRef}
+      className="my-10 relative"
+      style={{ minHeight: 269 }}
+    >
+      <div
+        className={cx('animate-pulse absolute w-full box-border', {
+          hidden: isLoaded,
+        })}
+        style={{ paddingLeft: 4, paddingRight: 4 }}
+      >
+        <div
+          className="h-5 rounded bg-gray-200"
+          style={{ marginLeft: 62, marginTop: 16, marginBottom: 16 }}
+        />
+        <div className="flex w-full">
+          <div
+            className="flex-shrink-0 rounded bg-gray-200"
+            style={{ width: 44, height: 44, marginRight: 16 }}
+          />
+          <div
+            className="flex-grow rounded bg-gray-200"
+            style={{ height: 196 }}
+          />
+        </div>
+      </div>
+    </div>
+  );
 });
 
 Comments.displayName = 'Comments';
