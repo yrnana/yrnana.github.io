4a1b805076dda1f5290b839cfc5d6d342419cce1
yrnana <nyryn0945@gmail.com>
2021-09-20 21:48:05 +0900

    Updates

--- _next/static/4jW-L9wIdjYi9uYSkL7i9/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Ftags","\u002Farchive","\u002Fabout","\u002F","\u002Ftag\u002F[tag]","\u002Fpage\u002F[page]","\u002Fpost\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ _next/static/5EYf5svBG8YnA-tMYFvtx/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fabout","\u002Ftags","\u002F","\u002Farchive","\u002Fpage\u002F[page]","\u002Ftag\u002F[tag]","\u002Fpost\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
