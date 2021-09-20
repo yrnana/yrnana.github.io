782bde1d0d29a0b6c8b82178c087a0b9df7b7302
yrnana <nyryn0945@gmail.com>
2021-09-20 22:28:59 +0900

    Updates

--- _next/static/lTk6-KMsHxI5gHuxX6zqq/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Farchive","\u002Ftags","\u002F","\u002Fabout","\u002Fpage\u002F[page]","\u002Ftag\u002F[tag]","\u002Fpost\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
