1dc0786da8b28fa2220f4dbb1ac4899a15ef819c
yrnana <nyryn0945@gmail.com>
2021-09-20 21:17:32 +0900

    feat: 코멘트 레포를 env로 제어하는 기능 추가

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,5 +1,7 @@
 export const BLOG_URL = process.env.NEXT_PUBLIC_BLOG_URL;
 export const BLOG_TITLE = process.env.NEXT_PUBLIC_BLOG_TITLE;
 export const AUTHOR_NAME = process.env.NEXT_PUBLIC_AUTHOR_NAME;
+export const COMMENT_ISSUE_REPO =
+  process.env.NEXT_PUBLIC_COMMENT_ISSUE_REPO || '';
 
 export const PAGE_SIZE = 10;
