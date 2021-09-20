79578dfd4df76770313733e6ed78ca68c1342fca
yrnana <nyryn0945@gmail.com>
2021-09-20 21:12:32 +0900

    feat: 코멘트 기능 추가

--- src/templates/PostTemplate.tsx
+++ src/templates/PostTemplate.tsx
@@ -1,5 +1,10 @@
+import dynamic from 'next/dynamic';
 import Layout from '~/components/layout/Layout';
 import Post from '~/components/post/Post';
+
+const Comments = dynamic(() => import('~/components/post/Comments'), {
+  ssr: false,
+});
 
 export interface PostTemplateProps {
   postDetail: PostDetail;
@@ -9,6 +14,7 @@ const PostTemplate: React.VFC<PostTemplateProps> = ({ postDetail }) => {
   return (
     <Layout>
       <Post {...postDetail} />
+      <Comments />
     </Layout>
   );
 };
