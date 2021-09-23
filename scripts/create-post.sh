d011bfb0319d3e07e5d864fcc1389bf6daf6d101
yrnana <nyryn0945@gmail.com>
2021-09-23 20:39:44 +0900

    feat: shell script 작성

---
+++ scripts/create-post.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# get variable from input
+read -p "title : " title
+read -p "excerpt : " excerpt
+read -p "tags : " tags
+date1=`date +%F`
+date2=`date +%FT%T+09:00`
+
+# create markdown file
+message2="$(tr [A-Z] [a-z] <<< "$title")"
+slug=$date1"--"${message2// /-}
+markdown="./_md/posts/$slug.md"
+touch $markdown
+
+# insert data to markdown
+echo "---" >> $markdown
+echo "title: '$title'" >> $markdown
+echo "date: '$date2'" >> $markdown
+echo "excerpt: '$excerpt'" >> $markdown
+if [ "$tags" != "" ]; then
+  echo "tags: [$tags]" >> $markdown
+fi
+echo "published: false" >> $markdown
+echo "---" >> $markdown
+
+# exit
+exit 0
