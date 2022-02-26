198d4152777cd6f41216e51f64e47c0ef392dd1b
yrnana <nyryn0945@gmail.com>
2022-02-26 21:26:24 +0000

    Deploying to gh-pages from @ yrnana/yrnana.github.io@f2956b508359e74c9554a567ceba0c8cbef92ff5 🚀

---
+++ component---src-templates-markdown-template-tsx-81759b91505cd3201d88.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkyrnana_github_io=self.webpackChunkyrnana_github_io||[]).push([[315],{2053:function(t,a,e){e.r(a);var n=e(5827),r=e(2721),i=e(7069),s=e(5893);a.default=function(t){var a=t.data.markdownRemark,e=a.frontmatter,c=e.title,u=e.excerpt;return(0,s.jsxs)(r.Z,{children:[(0,s.jsx)(n.Z,{title:c,description:u||""}),(0,s.jsx)("div",{className:"markdown",children:(0,i.i)(a.htmlAst)})]})}}}]);
+//# sourceMappingURL=component---src-templates-markdown-template-tsx-81759b91505cd3201d88.js.map
