1192964e7a4db5d5fa50ddb36ba0586934c8ce15
nana-na <nana.na@kakaopaycorp.com>
2023-01-29 23:44:19 +0900

    feat: comment 작성

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,5 @@
+export { default as BigArrowLeftSvg } from '~/assets/big-left-icon.svg?raw';
+export { default as BigArrowRightSvg } from '~/assets/big-right-icon.svg?raw';
 export { default as ExclamationCircleSvg } from '~/assets/exclamation-circle.svg?raw';
 export { default as ArrowLeftSvg } from '~/assets/left-icon.svg?raw';
 export { default as ArrowRightSvg } from '~/assets/right-icon.svg?raw';
